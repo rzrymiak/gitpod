ee85ef0767921c114a213b3b9ff958e8f3564c31
Adrien Thebo <adrien@lagrange-automation.io>
2022-06-10 18:04:51 +0000

    server: expose license ID in installation telemetry

--- components/gitpod-protocol/src/installation-admin-protocol.ts
+++ components/gitpod-protocol/src/installation-admin-protocol.ts
@@ -28,6 +28,7 @@ export interface TelemetryData {
     totalUsers: number;
     totalWorkspaces: number;
     totalInstances: number;
+    licenseId?: string;
 }
 
 export namespace InstallationAdmin {
